d742fa7a32194d02c93b7047aed37efa74b95ff6
Bradman Davis <bradman.davis@my.jcu.edu.au>
2020-07-06 11:52:19 +1000

    Add bonus challenge 1

--- C383/C383 - necklace string.py
+++ C383/C383 - necklace string.py
@@ -11,11 +11,28 @@ def main():
                 print("Success")
         else:
                 print("Fail")
+
+        print("Challenge 2 Testing")
+        sList = ["aaaaaaa", "abcabcabc", "a", ""]
+        for s in sList:
+                count = repeats(s)
+                print(s + " repeats " + str(count) + " times")
 	
 def same_necklace(necklaceA, necklaceB):
 	if len(necklaceA) != len(necklaceB):
 		return False
 	return necklaceA in necklaceB + necklaceB
 
+def repeats(s):
+        # I'd argue that "" repeats 0 times but the specification says it would return 1
+        if s == "":
+                 return 1
+        repeats = 0
+        sCopy = s
+        for i in range(len(s)):
+                if sCopy == (s[i:] + s[:i]):
+                        repeats += 1
+        return repeats
+        
 
 main()
